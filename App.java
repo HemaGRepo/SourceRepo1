e23fcfb6a58bd022840754dbe0ff4d00fde6beb6
HemaGRepo <hema.g@capgemini.com>
2018-06-13 16:21:01 +0530

    V2.0
     This fix was done to Close #3.

--- App.java
+++ App.java
@@ -1,17 +1,19 @@
 /**
- * Hello world!
+ * This code is used by Jenkins
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
+       //Create a loop variable
 	int i=0;
+       //Loop starts
 	for(i=0;i<5;i++)
 	{
         System.out.println( "Hello World!....");
 		System.out.println( ".............Jenkins Build..........");
-		System.out.println( "Success!...."+i);
-	}
+		System.out.println( "Success!...."+i); //Print the iteration id
+	} //Loop ends
     }
 }
